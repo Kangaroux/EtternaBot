78daca41288ea574569006c7bc01f1febb1c36c9
Jesse Roux <roux.jesse@gmail.com>
2019-08-02 01:37:38 -0400

    Added docker config for tests

--- Makefile
+++ Makefile
@@ -2,17 +2,24 @@
 
 default: db bot
 
-build:
-	docker-compose build
-
-stop:
-	docker-compose stop
-
 bot:
 	docker-compose up -d bot
 
 db:
 	docker-compose up -d db
 
+stop:
+	docker-compose stop
+
+testdb:
+	docker-compose -f docker-compose-test.yml up db-test
+
+# Starts the test database and runs the go tests using the test db. The test db is reset
+# when this is ran. Use `make testdb` to start up the test db without destroying the
+# volume to inspect data after a test
+test:
+	docker-compose -f docker-compose-test.yml up -V --build --exit-code-from bot-test
+
+# Runs migrations on the production database
 migrations:
 	docker-compose run bot ./migrate.sh
--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 # Applies the database migrations. Must be ran from within the `bot` docker container
 
-./bin/migrate \
-    -path migrations/ \
-    -database postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@db/$POSTGRES_DB?sslmode=disable \
-    up
+echo "Applying migrations..."
+
+for i in 1 2 3 4 5; do
+    ./bin/migrate \
+        -path migrations/ \
+        -database postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$DATABASE_HOST/$POSTGRES_DB?sslmode=disable \
+        up
+
+    if [ $? -eq 0 ]; then
+        echo "Migrations applied successfully."
+        exit 0
+    fi
+
+    echo "Failed. Retrying ($i of 5)..."
+
+    sleep 1
+done
+
+echo "Migrations could not be applied."
+exit 1
